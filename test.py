cb6f9879d0f8fb92f2bab8e887e8f36dabeae0e6
Kaushik-Iyer <84177184+Kaushik-Iyer@users.noreply.github.com>
2023-07-03 16:18:30 +0530

    Update test.py
    Implement multithreading, kafka queuing, and calculations

--- test.py
+++ test.py
@@ -1,102 +1,112 @@
 import socket
 import struct
+import threading
+from confluent_kafka import Producer
 
-#code run on terminal is:  java -Ddebug=true -Dspeed=1.0 -classpath feed-play-1.0.jar hackathon.player.Main dataset.csv 9011
+kafka_bootstrap_servers='localhost:9092'
+kafka_topic='option-data'
 
-# Define the server address and port
-server_address = ('localhost', 9011)
+producer=Producer({'bootstrap.servers':kafka_bootstrap_servers})
+def find_first_number_index(string):
+    for i, char in enumerate(string):
+        if char.isdigit():
+            return i
+    return -1  # Return -1 if no number is found in the string
 
-# Create a TCP/IP socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Set a timeout value of 10 seconds
-client_socket.settimeout(600)
-
-try:
-    # Connect to the server
-    client_socket.connect(server_address)
-
-    # Send the initial request
-    client_socket.sendall(b'\x01')  # Sending a single byte packet indicating readiness
-
-    # Receive the snapshot data from the server
-    snapshot_data = client_socket.recv(130)  # Assuming the snapshot data packet size is fixed at 130 bytes
-    print("Received snapshot data size:", len(snapshot_data))
-    print("Snapshot Data:", snapshot_data)
-
-    # Unpack the snapshot data
-    packet_size, symbol, sequence, timestamp, ltp, ltq, volume, bid_price, bid_qty, ask_price, ask_qty, open_interest, prev_close, prev_open_interest = struct.unpack(
-        '<I30sQQQQQQQQQQQQ', snapshot_data)
-
-    total = 1
-    # Decode the symbol string
+def get_slice_until_index(string, index):
+    if index >= 0:
+        return string[:index]
+    else:
+        return string
+def process_packet(packet):
+    try:
+        packet_size, symbol, sequence, timestamp, ltp, ltq, volume, bid_price, bid_qty, ask_price, ask_qty, open_interest, prev_close, prev_open_interest = struct.unpack(
+            '<I30sQQQQQQQQQQQQ', packet)
+    except struct.error as e:
+        print("Error unpacking fields:",e)
+        return
     symbol = symbol.decode('utf-8').rstrip('\x00')
 
-    # Print the unpacked fields
-    print("Unpacked snapshot data:")
-    print("Symbol:", symbol)
-    print("Sequence Number:", sequence)
-    print("Timestamp:", timestamp)
-    print("Last Traded Price (LTP):", ltp)
-    print("Last Traded Quantity (LTQ):", ltq)
-    print("Volume:", volume)
-    print("Bid Price:", bid_price)
-    print("Bid Quantity:", bid_qty)
-    print("Ask Price:", ask_price)
-    print("Ask Quantity:", ask_qty)
-    print("Open Interest:", open_interest)
-    print("Previous Close Price:", prev_close)
-    print("Previous Open Interest:", prev_open_interest)
+    index=find_first_number_index(symbol)
+    underlying=get_slice_until_index(symbol,index)
+    if index!=-1:
+        expiry_date=symbol[index:index+7]
+    else:
+        expiry_date='-'
 
-    # Continue receiving real-time updates from the server
+    if index!=-1:
+        type=symbol[-2:]
+    else:
+        type='-'
+
+    if type!='XX':
+        strike=int(symbol[index+7:-2])
+    else:
+        strike=0
+
+    if type=='CE': type='Call'
+    if type=='PE':type='Put'
+
+
+
+    packet_data = {
+        'symbol': symbol,
+        'underlying':underlying,
+        'expiry_date':expiry_date,
+        'strike-price':strike,
+        'type':type,
+        'sequence': sequence,
+        'timestamp': timestamp,
+        'ltp': ltp,
+        'ltq': ltq,
+        'volume': volume,
+        'bid_price': bid_price,
+        'bid_qty': bid_qty,
+        'ask_price': ask_price,
+        'ask_qty': ask_qty,
+        'open_interest': open_interest,
+        'prev_close': prev_close,
+        'prev_open_interest': prev_open_interest
+    }
+    print(packet_data)
+    producer.produce(kafka_topic,key=symbol,value=str(packet_data))
+    producer.flush()
+
+def receive_packets(client_socket):
     while True:
-        print("TOTAL PACKETS SEEN:", total)
-
         try:
             data = client_socket.recv(130)
         except socket.timeout:
-            # Handle timeout, no data received within the timeout period
-            print("Timeout occurred. No data received.")
+            print("Timeout occured")
+            break
+        except OSError as e:
+            print("Socket error:",e)
             break
 
-
-
-        # Unpack the real-time update data
-        packet_size = struct.unpack('<I', data[:4])[0]
-
-        if packet_size != 124:
-            print("Invalid packet size. Skipping packet:", packet_size)
+        packet_size=len(data)
+        if packet_size!=130:
+            print("Invalid packet size. Skipping packet, with size:", packet_size)
             continue
 
-        # Unpack the remaining fields
-        print(packet_size)
-        try:
-            symbol, sequence, timestamp, ltp, ltq, volume, bid_price, bid_qty, ask_price, ask_qty, open_interest, prev_close, prev_open_interest = struct.unpack(
-                '<30sQQQQQQQQQQQQ', data[4:])
-        except struct.error as e:
-            print("Error unpacking fields:", e)
-            continue
-        # Decode the symbol string
-        symbol = symbol.decode('utf-8').rstrip('\x00')
+        packet_thread = threading.Thread(target=process_packet, args=(data,))
+        packet_thread.start()
 
-        # Print the unpacked fields
-        print("Packet Size:", packet_size)
-        print("Symbol:", symbol)
-        print("Sequence Number:", sequence)
-        print("Timestamp:", timestamp)
-        print("Last Traded Price (LTP):", ltp)
-        print("Last Traded Quantity (LTQ):", ltq)
-        print("Volume:", volume)
-        print("Bid Price:", bid_price)
-        print("Bid Quantity:", bid_qty)
-        print("Ask Price:", ask_price)
-        print("Ask Quantity:", ask_qty)
-        print("Open Interest:", open_interest)
-        print("Previous Close Price:", prev_close)
-        print("Previous Open Interest:", prev_open_interest)
+server_address = ('localhost', 9011)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.settimeout(600)
+try:
+    client_socket.connect(server_address)
+    client_socket.sendall(b'\x01')
 
-        total += 1
+    snapshot_data = client_socket.recv(130)
+    print("Received snapshot data size:", len(snapshot_data))
+    print("Snapshot Data:", snapshot_data)
+
+    process_packet(snapshot_data)
+    receive_thread=threading.Thread(target=receive_packets,args=(client_socket,))
+    receive_thread.start()
+
+    receive_thread.join()
 
 finally:
-    # Close the client socket when done or in case of an error
     client_socket.close()
